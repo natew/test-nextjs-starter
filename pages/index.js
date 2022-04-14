bd8a2ae7e5ac48c0ed8e34b2d4be3b97353bfcde
natew <natewienert@gmail.com>
2022-04-14 08:28:19 -1000

    fix

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-// should break
+// should fix
 
-export default function Home() 
+export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
